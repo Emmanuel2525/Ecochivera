00ea2ea4313dcdd3a0a406a085b3824c7d2de712
Emmanuel2525 <merola01t@gmail.com>
2020-12-14 23:00:07 -0400

    secciones

--- ecochivera/js/ventana.js
+++ ecochivera/js/ventana.js
@@ -1,3 +1,3 @@
 function ventanaSecundaria (URL){ 
-window.open(URL,"ventana1","width=1280,height=720,scrollbars=NO,top=100px,left=200") 
+window.open(URL,"ventana1","width=360,height=640,scrollbars=NO") 
 			} 
