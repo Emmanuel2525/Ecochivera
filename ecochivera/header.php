6e25e87c16bfe28fced157cf9142a60d0880f0af
Emmanuel2525 <merola01t@gmail.com>
2020-12-19 15:42:14 -0400

    ...

--- ecochivera/header.php
+++ ecochivera/header.php
@@ -117,7 +117,7 @@
 								<div class="dropdown">
 									<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
 										<i class="fa fa-heart-o-"></i>
-										<span>$=1.050.531,34Bs</span>
+										<span>$=1.303.254,00Bs</span>
 										<div class="qtyy"></div> 
 									</a>
 								</div>
